eec2e9187d163f1f6b0a649cfab20a8e31617dfa
bengitl <108184312+bengitl@users.noreply.github.com>
2025-12-17 22:47:17 +0800

    Update github.js

--- src/config/github.js
+++ src/config/github.js
@@ -1,7 +1,8 @@
 module.exports = {
   appId: process.env.GITHUB_APP_ID,
   webhookSecret: process.env.GITHUB_WEBHOOK_SECRET,
-  privateKey: process.env.GITHUB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.GITHUB_PRIVATE_KEY,
   clientId: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
 };
+
