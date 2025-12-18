c91a7cc4c6783b386a65ea1c99eb1f6b5bca26a6
bengitl <108184312+bengitl@users.noreply.github.com>
2025-12-18 14:36:22 +0800

    Update modelscope.js

--- src/config/modelscope.js
+++ src/config/modelscope.js
@@ -1,8 +1,9 @@
 module.exports = {
   apiKey: process.env.MODELSCOPE_API_KEY,
-  model: 'Qwen/Qwen2.5-Coder-32B-Instruct', // 或其他 ModelScope 支持的模型
-  apiUrl: 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation',  //https://api-inference.modelscope.cn/v1/
+  model: 'mistral-large-latest', // 或其他 ModelScope 支持的模型
+  apiUrl: 'https://api.mistral.ai/v1/chat/completions',  //https://api-inference.modelscope.cn/v1/
 };
+
 
 
 
